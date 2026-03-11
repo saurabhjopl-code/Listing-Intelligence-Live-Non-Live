84f0ff41858d203d37a58fb94ce578ecd134143b
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-03-11 13:56:34 +0530

    Update tabRenderer.js

--- js/renderers/tabRenderer.js
+++ js/renderers/tabRenderer.js
@@ -1,17 +1,23 @@
-export function initTabs(callback){
+export function initTabs(onChange){
 
 const buttons = document.querySelectorAll(".tabs button");
 
 buttons.forEach(btn=>{
 
-btn.addEventListener("click",()=>{
+btn.onclick = ()=>{
+
+buttons.forEach(b=>b.classList.remove("active"));
+
+btn.classList.add("active");
 
 const tab = btn.dataset.tab;
 
-callback(tab);
+onChange(tab);
+
+};
 
 });
 
-});
+buttons[0].classList.add("active");
 
 }
