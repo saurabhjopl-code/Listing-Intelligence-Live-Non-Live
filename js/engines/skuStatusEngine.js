ce59f9212de933c6cc7bea808de14dc6b017ecb3
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-03-11 12:17:21 +0530

    Update skuStatusEngine.js

--- js/engines/skuStatusEngine.js
+++ js/engines/skuStatusEngine.js
@@ -1,1 +1,42 @@
+export function computeSkuStatus(catalog,listings){
 
+const listingIndex = {};
+
+listings.forEach(row=>{
+
+listingIndex[row.uniware_sku] = true;
+
+});
+
+
+return catalog.map(sku=>{
+
+const listed = !!listingIndex[sku.uniware_sku];
+
+let status = "LIVE";
+
+if(!listed && sku.stock > 0){
+
+status = "CRITICAL";
+
+}
+
+else if(!listed){
+
+status = "NON_LIVE";
+
+}
+
+return{
+
+...sku,
+
+listed,
+
+status
+
+};
+
+});
+
+}
