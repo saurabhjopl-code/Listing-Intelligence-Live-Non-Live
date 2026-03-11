833615b6217742fe7ef2297c6da3c733b5366aa8
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-03-11 12:35:43 +0530

    Update tableRenderer.js

--- js/renderers/tableRenderer.js
+++ js/renderers/tableRenderer.js
@@ -1,25 +1,31 @@
+let PAGE = 50;
+
 export function renderTable(containerId, columns, data){
 
 const container = document.getElementById(containerId);
+
+let visible = data.slice(0,PAGE);
 
 let html = "<table class='table'>";
 
 html += "<thead><tr>";
 
 columns.forEach(col=>{
+
 html += `<th>${col}</th>`;
+
 });
 
-html += "</tr></thead>";
+html += "</tr></thead><tbody>";
 
-html += "<tbody>";
-
-data.forEach(row=>{
+visible.forEach(row=>{
 
 html += "<tr>";
 
 columns.forEach(col=>{
+
 html += `<td>${row[col] || ""}</td>`;
+
 });
 
 html += "</tr>";
@@ -28,6 +34,16 @@ html += "</tr>";
 
 html += "</tbody></table>";
 
+html += `<button id="loadMoreBtn">Load More</button>`;
+
 container.innerHTML = html;
+
+document.getElementById("loadMoreBtn").onclick = () => {
+
+PAGE += 50;
+
+renderTable(containerId,columns,data);
+
+};
 
 }
