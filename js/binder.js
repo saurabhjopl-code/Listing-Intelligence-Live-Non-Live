81b38bc5fbd07b9ebdd90f398f9b13a3d6cd38fe
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-03-11 12:22:11 +0530

    Update binder.js

--- js/binder.js
+++ js/binder.js
@@ -8,6 +8,12 @@ import { computeSkuStatus } from "./engines/skuStatusEngine.js";
 
 import { computeStyleCoverage } from "./engines/styleCoverageEngine.js";
 
+import { buildStyleMpIndex } from "./engines/styleMpIndexEngine.js";
+
+import { getPartialStyles } from "./engines/missingSizeEngine.js";
+
+import { getCriticalSkus } from "./engines/criticalSkuEngine.js";
+
 import { renderSummary } from "./renderers/summaryRenderer.js";
 
 
@@ -22,6 +28,16 @@ const listings = buildListingMap(sheets);
 const skuStatus = computeSkuStatus(catalog,listings);
 
 const styleCoverage = computeStyleCoverage(skuStatus);
+
+const styleMpIndex = buildStyleMpIndex(catalog,listings);
+
+const partialStyles = getPartialStyles(styleMpIndex);
+
+const criticalSkus = getCriticalSkus(skuStatus);
+
+console.log("Partial Styles", partialStyles);
+
+console.log("Critical SKUs", criticalSkus);
 
 renderSummary(styleCoverage);
 
