6ab8e8016deb54e06ee878756398a0ffed40b296
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-03-11 13:48:13 +0530

    Update filterEngine.js

--- js/core/filterEngine.js
+++ js/core/filterEngine.js
@@ -1,5 +1,13 @@
-export function applyFilters(data,filters){
+export function applyFilters(data, filters){
 
-return data;
+return data.filter(row=>{
+
+if(filters.mp && row.mp !== filters.mp) return false;
+
+if(filters.category && row.category !== filters.category) return false;
+
+return true;
+
+});
 
 }
