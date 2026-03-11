e6439ab56e64734dcd6e90a2ae6fc957fe3f8868
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-03-11 12:16:59 +0530

    Update masterCatalogEngine.js

--- js/engines/masterCatalogEngine.js
+++ js/engines/masterCatalogEngine.js
@@ -1,1 +1,34 @@
+export function buildCatalog(sheets){
 
+const master = sheets.master_listings;
+
+const stock = sheets.uniware_stock;
+
+const stockMap = {};
+
+stock.forEach(row=>{
+
+stockMap[row.uniware_sku] = Number(row.stock || 0);
+
+});
+
+
+return master.map(row=>{
+
+return{
+
+uniware_sku:row.uniware_sku,
+
+styleid:row.styleid,
+
+size:row.size,
+
+category:row.category,
+
+stock:stockMap[row.uniware_sku] || 0
+
+};
+
+});
+
+}
