cdc644a50641225a13e5ec2ebe29e8ee65de5c1a
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-03-11 13:49:06 +0530

    Update filterRenderer.js

--- js/renderers/filterRenderer.js
+++ js/renderers/filterRenderer.js
@@ -1,23 +1,25 @@
-export function renderFilters(data){
+export function renderFilters(DATA, onChange){
 
 const bar = document.getElementById("filterBar");
 
-const mps = [...new Set(data.listings.map(r=>r.mp))];
-
-const categories = [...new Set(data.catalog.map(r=>r.category))];
+const mps = [...new Set(DATA.listings.map(r=>r.mp))];
+const cats = [...new Set(DATA.catalog.map(r=>r.category))];
 
 bar.innerHTML = `
 
 <select id="mpFilter">
 <option value="">All MP</option>
-${mps.map(m=>`<option>${m}</option>`).join("")}
+${mps.map(m=>`<option value="${m}">${m}</option>`).join("")}
 </select>
 
 <select id="catFilter">
 <option value="">All Category</option>
-${categories.map(c=>`<option>${c}</option>`).join("")}
+${cats.map(c=>`<option value="${c}">${c}</option>`).join("")}
 </select>
 
 `;
+
+document.getElementById("mpFilter").onchange = onChange;
+document.getElementById("catFilter").onchange = onChange;
 
 }
