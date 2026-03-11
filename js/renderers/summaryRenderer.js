912948d95ea762dd196f73821067f4c5022b99d2
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-03-11 12:17:42 +0530

    Update summaryRenderer.js

--- js/renderers/summaryRenderer.js
+++ js/renderers/summaryRenderer.js
@@ -1,1 +1,26 @@
+export function renderSummary(data){
 
+const grid = document.getElementById("summaryGrid");
+
+const total = data.length;
+
+const fullLive = data.filter(d=>d.live===d.total).length;
+
+const partial = data.filter(d=>d.live>0 && d.live<d.total).length;
+
+const nonLive = data.filter(d=>d.live===0).length;
+
+
+grid.innerHTML = `
+
+<div class="card">Total Styles : ${total}</div>
+
+<div class="card">Full Live : ${fullLive}</div>
+
+<div class="card">Partial Live : ${partial}</div>
+
+<div class="card">Non Live : ${nonLive}</div>
+
+`;
+
+}
