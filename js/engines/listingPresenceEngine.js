9ec4158f64240ed2d2041bcb95efb304fc595fcd
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-03-11 14:02:53 +0530

    Update listingPresenceEngine.js

--- js/engines/listingPresenceEngine.js
+++ js/engines/listingPresenceEngine.js
@@ -1,28 +1,29 @@
 export function buildListingMap(sheets){
 
 const data = sheets.data;
-
 const channel = sheets.channel_name;
 
 const channelMap = {};
 
 channel.forEach(row=>{
 
-channelMap[row.channel_name] = row.mp;
+channelMap[row.channel_name] = {
+mp: row.mp,
+account: row.account
+};
 
 });
 
-
 return data.map(row=>{
+
+const mapping = channelMap[row.channel_name] || {};
 
 return{
 
 uniware_sku:row.uniware_sku,
-
-mp:channelMap[row.channel_name] || "Unknown",
-
+mp:mapping.mp || "",
+account:mapping.account || "",
 mp_sku:row.mp_sku,
-
 pid:row.pid
 
 };
